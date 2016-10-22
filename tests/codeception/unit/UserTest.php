8968250f0bd03c97a408cad9c751d96f1dc97521
Tobias Munk <schmunk@usrbin.de>
2016-10-22 18:35:06 +0200

    updated unit test order

--- tests/codeception/unit/UserTest.php
+++ tests/codeception/unit/UserTest.php
@@ -10,15 +10,15 @@ class UserTest extends TestCase
 
     public $appConfig = '@tests/codeception/_config/test.php';
 
-    public function testUserLogout()
-    {
-        $this->assertTrue(\Yii::$app->user->logout());
-    }
-
     public function testUserLogin()
     {
         $identity = User::findIdentity(1);
         $this->assertTrue(\Yii::$app->user->login($identity, 3600));
+    }
+
+    public function testUserLogout()
+    {
+        $this->assertTrue(\Yii::$app->user->logout());
     }
 
     public function testNonExistingUserModel()
